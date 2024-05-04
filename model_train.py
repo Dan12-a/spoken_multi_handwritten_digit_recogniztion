3df7aa1ccc9ce0cbc9e91db7cbf1321f62a525d3
DANIYA TABASUM <119096073+Dan12-a@users.noreply.github.com>
2024-05-04 15:01:56 +0530

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -1,41 +1,63 @@
-from keras.datasets import mnist
+from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pickle
+import matplotlib.pyplot as plt
 
+# Load and preprocess the data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
-X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
-
-X_train = X_train / 255
-X_test = X_test / 255
-
+X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32') / 255.0
+X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32') / 255.0
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 num_classes = y_test.shape[1]
 
+# Data Augmentation
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    zoom_range=0.1
+)
+datagen.fit(X_train)
 
+# Define the model
 def myModel():
     model = Sequential()
-    model.add(Conv2D(6, (5, 5), input_shape=(28, 28, 1), activation="relu"))
+    model.add(Conv2D(32, (3, 3), input_shape=(28, 28, 1), activation='relu'))
     model.add(MaxPooling2D())
-    model.add(Conv2D(16, (5, 5), activation="relu"))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D())
     model.add(Flatten())
-    model.add(Dense(120, activation="relu"))
-    model.add(Dense(84, activation="relu"))
-    model.add(Dense(num_classes, activation="softmax"))
-
-    #compilation
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
+# Create and train the model
 model = myModel()
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=128)
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=128),
+                              steps_per_epoch=len(X_train) / 128, epochs=25,
+                              validation_data=(X_test, y_test))
 
-pickle_out = open("trained_model.p", "wb")
-pickle.dump(model, pickle_out)
-pickle_out.close()
+# Evaluate the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Test Loss:", scores[0])
+print("Test Accuracy:", scores[1])
+
+# Save the trained model
+with open("trained_model.p", "wb") as pickle_out:
+    pickle.dump(model, pickle_out)
+
+# Plot the training and validation accuracy
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+plt.show()
+
